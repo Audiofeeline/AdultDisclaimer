47ca22d1323ddd03bf685d3ca057d43e44519d6e
Audiofeeline <contact@audiofeeline.net>
2017-07-01 11:13:03 +0200

    Update disclaimer.php

--- disclaimer.php
+++ disclaimer.php
@@ -1,12 +1,9 @@
 <?php
-// Remember : do not code when drunk.
-
 session_start();
-if (!isset($_SESSION['disclaimersession']) {$_SESSION['disclaimersession']=TRUE;} 
-if ($_SESSION['disclaimersession']==TRUE){
-  
-echo '<div style="z-index:666;width:100%;background-color:#000">Disclaimer : For the glory of satan.</div>';
-  
-}
+if (!isset($_SESSION['count'])) {$_SESSION['count'] = 1;} else {$_SESSION['count']++;}
+if ($_SESSION['count'] <= 1){
 
+echo 'DISCLAIMER';
+
+}
 ?>
