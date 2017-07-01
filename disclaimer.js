f9cf4cc42f0172c2d44be6d612c53f850525cff8
Audiofeeline <contact@audiofeeline.net>
2017-07-01 13:40:07 +0200

    Update disclaimer.js

--- disclaimer.js
+++ disclaimer.js
@@ -8,8 +8,8 @@ document.cookie = 'cookiename=disclaimer; expires=' + disclaimerdate.toGMTString
 if(expires != ""){
 document.write('<div id="disclaimer">');
 document.write('<p>Bla Bla Bla Bla</p>');
-document.write('<a href="#" onClick="javascript:document.getElementById(\'disclaimer\').style.display=\'none\';disclaimercookie();">Entrer</a> - <a href="">Sortir</a>);
-document.write('</div>');
+document.write('<a href="#" onClick="javascript:document.getElementById(\'disclaimer\').style.display=\'none\';disclaimercookie()">Entrer</a> - <a href="">Sortir</a>);
+document.write('</div>')
 }
 
-else {document.getElementById(\'disclaimer\').style.display=\'none\';}
+else {document.getElementById(\'disclaimer\').style.display=\'none\'}
