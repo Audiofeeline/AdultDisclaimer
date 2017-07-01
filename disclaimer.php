50f7332446e16e68bb121552deff93f14261af85
Audiofeeline <contact@audiofeeline.net>
2017-07-01 10:41:26 +0200

    Update disclaimer.php

--- disclaimer.php
+++ disclaimer.php
@@ -1,4 +1,6 @@
 <?php
+// Remember : do not code when drunk.
+
 // Showing disclaimer once per session.
 session_start();
 if (!isset($_SESSION['count'])) {$_SESSION['count'] = 1;} else {$_SESSION['count']++;}
