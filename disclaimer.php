ba20b9c436724003217e45eaa36282c514973fb9
Audiofeeline <contact@audiofeeline.net>
2017-07-01 10:41:00 +0200

    Update disclaimer.php

--- disclaimer.php
+++ disclaimer.php
@@ -3,7 +3,7 @@
 session_start();
 if (!isset($_SESSION['count'])) {$_SESSION['count'] = 1;} else {$_SESSION['count']++;}
 if ($_SESSION['count'] <= 1){
-echo 'DISCLAIMER';}
+echo '<div style="z-index:666">Disclaimer : For the glory of satan.</div>';}
 
 // Now we have to deal with cached websites.
 
