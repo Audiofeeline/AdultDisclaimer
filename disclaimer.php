744912e931a4dbc0d8a542e43342558efa18eeaa
Audiofeeline <contact@audiofeeline.net>
2017-07-01 10:36:27 +0200

    Update disclaimer.php

--- disclaimer.php
+++ disclaimer.php
@@ -3,4 +3,7 @@ session_start();
 if (!isset($_SESSION['count'])) {$_SESSION['count'] = 1;} else {$_SESSION['count']++;}
 if ($_SESSION['count'] <= 1){
 echo 'DISCLAIMER';}
+
+// Now we have to deal with cached websites.
+
 ?>
