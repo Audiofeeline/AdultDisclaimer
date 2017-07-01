3360f1a087516e9463ab9208d2e6f1e69019aa76
Audiofeeline <contact@audiofeeline.net>
2017-07-01 13:34:34 +0200

    Update and rename disclaimer.php to disclaimer.js

---
+++ disclaimer.js
@@ -0,0 +1,13 @@
+function disclaimercookie(){
+disclaimerdays=1;
+disclaimerdate = new Date();
+disclaimerdate.setTime(disclaimerdate.getTime()+(disclaimerdays*24*60*60*1000));
+document.cookie = 'cookiename=disclaimer; expires=' + disclaimerdate.toGMTString();
+}
+
+if(expires != ""){
+document.write('<div id="disclaimer">);
+document.write('<p>Bla Bla Bla Bla</p>);
+document.write('<a href="#" onClick="javascript:document.getElementById(\'disclaimer\').style.display=\'none\';disclaimercookie();">Entrer</a> - <a href="">Sortir</a>);
+document.write('</div>');
+}
